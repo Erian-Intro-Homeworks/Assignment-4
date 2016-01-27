66d5f40ab1d4d21f0a9668072b8ec27fda68269f
Erian Vazquez <erian.vazquez@gmail.com>
2016-01-26 23:32:40 -0500

    added diagram

--- server/config/config.example.js
+++ server/config/config.example.js
@@ -1,11 +1,11 @@
-//This file holds any configuration variables we may need 
+//This file holds any configuration variables we may need
 //'config.js' is ignored by git to protect sensitive information, such as your database's username and password
 //copy this file's contents to another file 'config.js' and store your MongoLab uri there
 
 module.exports = {
   db: {
-    uri: 'mongodb://Erian:Erian1@ds047365.mongolab.com:47365/uf-s16-3b', //place the URI of your mongo database here.
-  }, 
+    uri: '', //place the URI of your mongo database here.
+  },
   googleMaps: {
     key: ''
   },
